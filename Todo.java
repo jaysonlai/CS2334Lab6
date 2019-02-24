b239b2b1a5488fd51e975e423a37fc9261453416
Jayson <jayson.k.lai-1@ou.edu>
2019-02-24 17:39:26 -0600

    Beginning of project

---
+++ Todo.java
@@ -0,0 +1,27 @@
+public class Todo {/*
+Lab Section_________: 012
+Last Modified_______: 2019-02-24
+OU ID_______________: 113450461
+OU EMAIL____________: jayson.k.lai-1@ou.edu
+
+Tasks:
+1. XXX
+2. XXX
+3. 
+4. 
+5. 
+
+Estimated Time (minutes):
+1. XXX
+2. XXx
+3. 
+4. 
+5. 
+
+Actual Time (minutes):
+1. XXX
+2. XXX
+3. 
+4. 
+5. 
+*/}
