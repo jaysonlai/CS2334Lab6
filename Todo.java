f3a6bd14325de26785fe9f94484626ef57a17c9c
Jayson <jayson.k.lai-1@ou.edu>
2019-02-26 14:32:08 -0600

    copied over correct todo file

--- Todo.java
+++ Todo.java
@@ -5,23 +5,23 @@ OU ID_______________: 113450461
 OU EMAIL____________: jayson.k.lai-1@ou.edu
 
 Tasks:
-1. XXX
-2. XXX
-3. 
-4. 
-5. 
+1. Finish the defining the child classes of Shape
+2. Create Rectangle and Square classes
+3. Finish defining the comparator classes
+4. Complete the shapeSorter Classes
+5. Check for comparisons to see if correctly runs
 
 Estimated Time (minutes):
-1. XXX
-2. XXx
-3. 
-4. 
-5. 
+1. 40
+2. 5
+3. 40
+4. 40
+5. 30
 
 Actual Time (minutes):
-1. XXX
-2. XXX
-3. 
-4. 
-5. 
+1. 60
+2. 3
+3. 60
+4. 120
+5. 40
 */}
